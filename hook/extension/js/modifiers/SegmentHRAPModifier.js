ec12b594029a3af16e72c54a085cffef3d08f3ab
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2016-09-01 13:06:16 +0200

    Added HRAP column to data rows (no content yet).

--- hook/extension/js/modifiers/SegmentHRAPModifier.js
+++ hook/extension/js/modifiers/SegmentHRAPModifier.js
@@ -31,6 +31,25 @@ SegmentHRAPModifier.prototype = {
 
         this.pace.after('<th class="hrap">HRAP</th>');
 
+        results.find("tbody").find("tr").appear().on("appear", function(e, $items) {
+
+            $items.each(function() {
+
+                var $row = $(this),
+                    $cells = $row.find("td"); // cells are: Rank, Name, Date, Pace, **HRAP**, HR, VAM, Time
+
+                if ($cells.filter('.hrap_pace').size()==0) {
+                    var pace = $cells.eq(3);
+
+                    pace.after('<td class="hrap_pace">?</td>');
+                }
+
+            });
+
+        });
+
+        $.force_appear();
+
         if (resultsHeader.find('.hrap').size()) {
             clearInterval(self.hrapLoop);
         }
