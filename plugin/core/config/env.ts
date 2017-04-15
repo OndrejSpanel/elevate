a28ab4f0b19e12a079af3b59985203ad3d1784fc
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-04-15 16:01:30 +0200

    Debug mode settings.

--- plugin/core/config/env.ts
+++ plugin/core/config/env.ts
@@ -2,7 +2,7 @@ let env: Env = {
     preview: false, // Must be false in release
     analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
     simulateUpdate: false, // Must be false in release
-    debugMode: false, // Must be false in release
+    debugMode: true, // Must be false in release
     useActivityStreamCache: true, // Must be true in release
     endPoint: 'https://stravistix-thomaschampagne.rhcloud.com' // StravistiX endPoint for new features
 };
